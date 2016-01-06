20e7f0c16e2c738e0e6b4b502073bfab980481b4
cc2RG <rbgalloway88@gmail.com>
2016-01-06 19:31:01 +0000

    new commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def role?(role_to_compare)       
+    self.role.to_s == role_to_compare.to_s
+  end  
   has_many :comments
   
   has_many :articles
